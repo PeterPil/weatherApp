04f52a29bd10ffd993a7e10bce54c462c27bcc06
Peter <megolko@gmail.com>
2019-05-11 08:25:51 +0300

    fix render data

--- src/components/WeatherForOneDay.js
+++ src/components/WeatherForOneDay.js
@@ -2,8 +2,9 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import connect from "react-redux/es/connect/connect";
 import React from "react";
-import { Carousel, RoutedCarousel } from "./Carousel";
-import * as weatherActions from "../actions/weatherActions";
+import { RoutedCarousel } from "./Carousel";
+import {weatherActions} from "../actions";
+import {format} from "date-fns";
 
 class WeatherForOneDay extends Component {
   componentDidMount() {
@@ -11,15 +12,20 @@ class WeatherForOneDay extends Component {
   }
   render() {
     const data = this.props.weatherList.filter(
-      item => item.dt_txt.split(" ")[0] === this.props.match.params.date
+      item => {
+        console.log(format(item.dt_txt, "YYYY-DD-HH"), this.props.match.params.date);
+        // console.log(item.dt_txt);
+        return format(item.dt_txt, "YYYY-DD-HH") === this.props.match.params.date
+      }
     );
+    // console.log(format(new Date(this.props.match.params.date), "YYYY-DD-HH"));
     return (
       <RoutedCarousel
         weather={{
           list: data,
           city: this.props.city,
           weatherType: "today",
-          weatherDay: data[0].dt_txt
+          weatherDay: format(new Date(this.props.match.params.date), "YYYY-DD-HH")
         }}
         isError={this.props.isError}
       />
